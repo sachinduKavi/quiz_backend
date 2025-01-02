4675cf3d5eb94a8a642587b112116f845c0ca744
sachinduKavi <cst21017@std.uwu.ac.lk>
2025-01-02 23:38:50 +0530

    user class

--- user/controller.py
+++ user/controller.py
@@ -1,11 +1,67 @@
-from django.http import HttpResponse
-
+from django.http import JsonResponse, HttpResponse, HttpRequest
+from django.views.decorators.http import require_POST
+from .models import user_collection
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 
 class Controller:
     
+    
     # New user registration
+    @require_POST
+    @csrf_exempt
+    @staticmethod
     def userRegistration(request):
-        print('user registration...')
+        proceed = False 
+        content = None
+        message = None
+        body = json.loads(request.body)
         
-        return HttpResponse("Hello WOrld")
+        
+        user_details = {
+            "name": body.get('name'),
+            "email": body.get('email'),
+            "password": body.get('password')
+        }
+        
+        try:
+            db_response = user_collection.insert_one(user_details)
+            if db_response.acknowledged:
+                proceed = True
+        except Exception as e:
+            print(e)
+            message = e
+        
+        
+        
+        # Response to the client
+        return JsonResponse({
+            "proceed": proceed,
+            "content": content,
+            "message": message
+        })
+        
+        
+        
+    # Check user credentials 
+    @require_POST
+    @csrf_exempt
+    @staticmethod
+    def authentication(request): 
+        proceed = False 
+        content = None
+        message = None
+        body = json.loads(request.body)
+        
+        
+        
+        
+        
+        # Response to the client
+        return JsonResponse({
+            "proceed": proceed,
+            "content": content,
+            "message": message
+        })
--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from quiz_backend.connection_db import db
 
 # Create your models here.
+user_collection = db['User']
+
+class User:
+    def __init__(self, _id = None, name = None, email = None, password = None):
+        self._id = _id
+        self.name = name
+        self.email = email
+        self.password = password
+    
+    
